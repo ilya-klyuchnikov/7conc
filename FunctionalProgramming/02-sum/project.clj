0d3d4b63a23855731dd941ee06dd1a19542ff13b
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2017-03-01 07:34:28 +0000

    FP (part 1)

--- FunctionalProgramming/02-sum/project.clj
+++ FunctionalProgramming/02-sum/project.clj
@@ -1,11 +1,11 @@
 ;---
 ; Excerpted from "Seven Concurrency Models in Seven Weeks",
 ; published by The Pragmatic Bookshelf.
-; Copyrights apply to this code. It may not be used to create training material, 
+; Copyrights apply to this code. It may not be used to create training material,
 ; courses, books, articles, and the like. Contact us if you are in doubt.
-; We make no guarantees that this code is fit for any purpose. 
+; We make no guarantees that this code is fit for any purpose.
 ; Visit http://www.pragmaticprogrammer.com/titles/pb7con for more book information.
 ;---
 (defproject fp-sum "1.0"
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :main sum.core)
